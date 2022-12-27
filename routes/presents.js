a8ceec75ccee06a9475b2ea8943e6d0f642b62fb
Pau Bertolin <paulaberos@gmail.com>
2022-12-27 17:30:21 +0100

    modificate DB - image url

--- routes/presents.js
+++ routes/presents.js
@@ -8,9 +8,21 @@ router.get('/', async (req, res, next) => {
   try {
     const presents = await Present.find({});
     res.render('presents', { presents });
+    return Present.updateOne({name: "The Colour Monster"}, {$set: {image:"/images/colour-monster-book-1.jpeg"}})
   } catch (error) {
     next(error)
+  } 
+});
+
+/* GET Present Detail page */
+/* ROUTE /presents/:id page */
+router.get('/:presentId', async (req, res, next) => {
+  const { presentId } = req.params;
+  try {
+    const present = await Present.findById(presentId);
+    res.render('presentsDetails', present)
+  } catch (error){
+    next(error)
   }
- 
 });
 module.exports = router;
