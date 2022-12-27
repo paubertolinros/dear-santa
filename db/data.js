8bcebd85758ddffa67ce4e1aa94fbec7c607de5d
Pau Bertolin <paulaberos@gmail.com>
2022-12-27 13:28:04 +0100

    Create data file in db folder

---
+++ db/data.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    name: 'The Colour Monster',
+    image: '/images/colour-monster-book.jpeg',
+    price: 16,
+    recipient: 'Lila',
+    season: 'christmas',
+    description: 'We teach toddlers to identify colors, numbers, shapes, and letters— but what about their feelings? By illustrating such common emotions as happiness, sadness, anger, fear, and calm, this sensitive book gently encourages young children to open up with parents, teachers, and daycare providers. And kids will LOVE the bright illustrations on every page!'
+  },
+  {
+    name: 'Fuji Bike',
+    image: '/images/bike-white.jpeg',
+    price: 200,
+    recipient: 'Carla',
+    season: 'birthday',
+    description: "Fast, Fancy, and Functional. The Feather is an all-purpose single-speed commuter or urban utility bike that might just be the most fun bike you've ever ridden. A flip-flop hub puts a fixed gear cog on one side and a freewheel on the other so you can choose your riding style."
+  },
+  {
+    name: 'Lettering Kit',
+    image: '/images/lettering.jpeg',
+    price: 15,
+    recipient: 'Carla',
+    season: 'invisible friend',
+    description: 'Perfect for beginners and those eager to learn classic calligraphy, Creative Hand Lettering includes all the information and materials you need to begin crafting gorgeous and ageless hand-lettered alphabets. Beautiful step-by-step projects demonstrate how to use the letters you have learned, while creative prompts and unique exercises inspire you to take your lettering to the next level.'
+  },
+  {
+    name: 'Polaroid Now i-Type Instant Camera',
+    image: '/images/pola.jpg',
+    price: 99,
+    recipient: 'Albert',
+    season: 'christmas',
+    description: "Capture and keep your everyday moments forever with the Polaroid Now. Our new analog instant camera comes with autofocus to help you catch life as you live it in that iconic Polaroid instant film format. In 7 colors, plus black and white, there’s a Polaroid Now to suit you. Compatible with i-Type & 600 film."
+  },
+  {
+    name: 'Backgammon cocktail tray',
+    image: '/images/backgammon.jpg',
+    price: 50,
+    recipient: 'Pere',
+    season: 'christmas',
+    description: 'Designed with style in mind, the acrylic Backgammon Cocktail Tray is perfect for entertaining. Made from transparent acrylic and complete with white, transparent and sepia-toned playing pieces, this modern 2-in-1 drinks tray includes everything you need for an afternoon game of backgammon by the pool. Bring a sophisticated edge to your next pool party or sunset sipping session with this classic game set reimagined.'
+  },
+]
